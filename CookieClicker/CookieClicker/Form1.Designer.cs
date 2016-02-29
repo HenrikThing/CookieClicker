8ec3e6bb05be9c02b7339b8dfa2f16aac7fcd66e
NielsJustesen <niels449@hotmail.com>
2016-02-29 13:05:58 +0100

    stupid picture

--- CookieClicker/CookieClicker/Form1.Designer.cs
+++ CookieClicker/CookieClicker/Form1.Designer.cs
@@ -43,9 +43,9 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(487, 38);
+            this.pictureBox1.Location = new System.Drawing.Point(635, 31);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(194, 155);
+            this.pictureBox1.Size = new System.Drawing.Size(118, 103);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
